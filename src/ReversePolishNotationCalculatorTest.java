cac39adad07c1b384de2280cacbc3d121122c9d3
Svyatoslav Lyskov <justerhofman25@yandex.ru>
2023-03-03 22:55:07 +0300

    reversed polish notation test

---
+++ src/ReversePolishNotationCalculatorTest.java
@@ -0,0 +1,82 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReversePolishNotationCalculatorTest {
+
+    @Test
+    public void shouldCalculateAddition() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        int result = calculator.calculatePolishNotation("1 2 +");
+        assertEquals(3,result);
+    }
+
+    @Test
+    public void shouldCalculateDecreasing() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        int result = calculator.calculatePolishNotation("1 2 -");
+        assertEquals(-1,result);
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        int result = calculator.calculatePolishNotation("1 2 *");
+        assertEquals(2,result);
+    }
+
+}
+
+class ReversePolishNotationCalculator {
+
+    public int calculatePolishNotation(String str) {
+        String[] parts = str.split(" ");
+        Deque<Integer> numbers = new ArrayDeque<>();
+        int index = 0;
+
+        while (index != parts.length) {
+
+            if (parts[index].isBlank()) {
+                index++;
+                continue;
+            }
+
+            if (isOperation(parts[index])) {
+                int operandOne = numbers.pop();
+                int operandTwo = numbers.pop();
+
+                switch (parts[index]) {
+                    case "+":
+                        numbers.push(operandOne + operandTwo);
+                        break;
+                    case "-":
+                        numbers.push(operandTwo - operandOne);
+                        break;
+                    case "*":
+                        numbers.push(operandOne * operandTwo);
+                        break;
+                }
+
+            } else {
+                numbers.push(Integer.parseInt(parts[index]));
+            }
+
+            index++;
+        }
+
+        return numbers.pop();
+    }
+
+    private boolean isOperation(String part) {
+        if (part.equals("+")
+                || part.equals("-")
+                || part.equals("*")) {
+            return true;
+        }
+
+        return false;
+    }
+}
